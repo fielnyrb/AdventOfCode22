fdf71bca012af5268ec6e21d16c225621e816092
fielnyrb <brynleif@brynleif.com>
2022-12-02 12:56:21 +0000

    Day 2

---
+++ AdventOfCode22/AdventOfCode22/Day2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode22
+{
+    class Day2
+    {
+        public int part1()
+        {
+            List<string> originalInput = File.ReadAllLines(@"input\day2.txt").ToList();
+
+            int totalScore = 0;
+
+            for (int i = 0; i < originalInput.Count(); i++)
+            {
+                string opponentMove = originalInput[i].Split(" ")[0];
+                string myMove = originalInput[i].Split(" ")[1];
+
+                int selectedShapePoint = mapShape(myMove);
+
+                totalScore += (selectedShapePoint + RockPaperScissor(myMove, opponentMove));
+            }
+
+            return totalScore;
+        }
+
+        public int part2()
+        {
+            List<string> originalInput = File.ReadAllLines(@"input\day2.txt").ToList();
+
+            int totalScore = 0;
+
+            for (int i = 0; i < originalInput.Count(); i++)
+            {
+                string opponentMove = originalInput[i].Split(" ")[0];
+                string myMove = GetMove(originalInput[i].Split(" ")[1], opponentMove);
+
+                int selectedShapePoint = mapShape(myMove);
+
+                totalScore += (selectedShapePoint + RockPaperScissor(myMove, opponentMove));
+            }
+
+            return totalScore;
+        }
+
+        int mapShape(string shape) {
+            if (shape == "X") return 1;
+            if (shape == "Y") return 2;
+            if (shape == "Z") return 3;
+            return 0;
+        }
+
+        int RockPaperScissor(string myMove, string opponentMove) {
+            if (myMove == "X" && opponentMove == "A") return 3;
+            if (myMove == "Y" && opponentMove == "B") return 3;
+            if (myMove == "Z" && opponentMove == "C") return 3;
+
+            if ((myMove == "X" && opponentMove == "C") ||
+                (myMove == "Y" && opponentMove == "A") ||
+                (myMove == "Z" && opponentMove == "B")) return 6; 
+            return 0;
+        }
+
+        string GetMove(string outcome, string opponentMove)
+        {
+            if(outcome == "X")
+            {
+                if (opponentMove == "A") return "Z";
+                if (opponentMove == "B") return "X";
+                if (opponentMove == "C") return "Y";
+            }
+            if (outcome == "Y")
+            {
+                if (opponentMove == "A") return "X";
+                if (opponentMove == "B") return "Y";
+                if (opponentMove == "C") return "Z";
+            }
+            if (outcome == "Z")
+            {
+                if (opponentMove == "A") return "Y";
+                if (opponentMove == "B") return "Z";
+                if (opponentMove == "C") return "X";
+            }
+            return "";
+        }
+    }
+}
--- AdventOfCode22/AdventOfCode22/Program.cs
+++ AdventOfCode22/AdventOfCode22/Program.cs
@@ -9,28 +9,8 @@ namespace AdventOfCode22
     {
         static void Main(string[] args)
         {
-            List<string> originalInput = File.ReadAllLines(@"input\day1.txt").ToList();
-            List<int> counts = new List<int>();
-
-            counts.Add(0);
-
-            for (int i = 0; i < originalInput.Count(); i++)
-            {
-                if (originalInput[i] == "")
-                {
-                    counts.Add(0);
-                }
-                else
-                {
-                    int countsIndex = counts.Count() == 0 ? 0 : counts.Count() - 1;
-                    counts[countsIndex] += int.Parse(originalInput[i]);
-                }
-            }
-            counts.Sort();
-            int top1 = counts[counts.Count() - 1];
-            int top2 = counts[counts.Count() - 2];
-            int top3 = counts[counts.Count() - 3];
-            Console.WriteLine(top1+top2+top3);
+            Day2 d2 = new Day2();
+            Console.WriteLine(d2.part2());
         }
     }
 }
